345aecc9b7870791b3c66abc6baff97ce15d3e5e
Walid JARRAH <walid.jarrah@protonmail.com>
2021-09-06 00:16:41 +0200

    Suite Init du projet

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ from lime import lime_tabular
 ## Data Analysis
 from tools.utils import data_analysis
 
-
+import warnings
+warnings.filterwarnings("ignore")
 
 
 
@@ -30,13 +31,22 @@ from tools.utils import data_analysis
 dtf = pd.read_csv('input_data/data_titanic.csv')
 dtf.head()
 
-# data_analysis.dtf_overview(dtf)
-# data_analysis.freqdist_plot(dtf, "Embarked")
-# data_analysis.bivariate_plot(dtf, x="Age", y="Fare")
-# data_analysis.nan_analysis(dtf, "Sex", "Age")
-# data_analysis.cross_distributions(dtf, "Age", "Survived", "Sex")
-# data_analysis.corr_matrix(dtf)
-data_analysis.pps_matrix(dtf)
+
+
+data_analysis.dtf_overview(dtf)
+dtf = dtf.set_index("PassengerId")
+dtf = dtf.rename(columns={"Survived":"Y"})
+
+features=[]
+data_analysis.freqdist_plot(dtf, "Y", figsize=(5, 3))
+data_analysis.corr_matrix(dtf, method="pearson", negative=False, lst_filters=["Y"], figsize=(15, 1))
+pps = data_analysis.pps_matrix(dtf, lst_filters=["Y"], figsize=(15,1))
+data_analysis.bivariate_plot(dtf, x="Sex", y="Y", figsize=(10, 5))
+# coeff, p = data_analysis.test_corr(dtf, x="Sex", y="Y")
+# data_analysis.nan_analysis(dtf, na_x="Age", y="Y", max_cat=20, figsize=(10,5))
+# data_analysis.cross_distributions(dtf, "Age", "Y", "Sex")
+# plt.show()
+
 
 
 
